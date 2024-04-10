3b07c929999384d27cc43df5ac5fe830565f2ddf
Avani Mathur <151984867+avanimathur@users.noreply.github.com>
2024-04-10 18:02:58 +0530

    Update scriptDashboard.js

--- navigateToPages/scriptDashboard.js
+++ navigateToPages/scriptDashboard.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const signUpBtn = document.querySelector('.sign-up');
     signUpBtn.addEventListener('click', function () {
-        window.location.href = "/navigateToPages/dashboard.html";
+        window.location.href = "./navigateToPages/dashboard.html";
     });
 
     const logInBtn = document.querySelector('.log-in');
     logInBtn.addEventListener('click', function () {
-        window.location.href = "/navigateToPages/dashboard.html";
+        window.location.href = "./navigateToPages/dashboard.html";
     });
 });
